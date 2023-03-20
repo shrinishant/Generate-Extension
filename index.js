75bb77a0a0c1ac53a277df6e3efeb8da434884d3
shrinishant <nishantkumaranaspiringiitian@gmail.com>
2023-03-21 01:48:06 +0530

    basic manifest.json generator

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import chalk from 'chalk'
 import clear from 'clear'
 import figlet from 'figlet'
 
+// lib
+import {isDirectoryExists, getCurrentDirectory} from './lib/files.js'
+import {createManifest} from './lib/manifest.js'
+import {askQuestions} from './lib/inquirer.js'
 
 clear()
 
@@ -10,3 +14,10 @@ console.log(
         figlet.textSync("Create Extension", {horizontalLayout: 'full'})
     )
 )
+
+if(isDirectoryExists('./manifest.json')){
+    console.log(chalk.red('Already a extension Directory'))
+    process.exit()
+}
+
+createManifest()
